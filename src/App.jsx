d470fb881d652a2192c2ff26b745ffacd8d04867
Mohamed Ashraf <Mohamed-Ashraf186>
2026-07-04 18:47:57 +0300

    Implement basic TODO list functionality with item input and display; update styles and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import ItemInput from "./ItemInput";
 import ItemList from "./ItemList";
 function App() {
+
+  const [items,setItems] = useState([]);
+
+
+  console.log(items);
+  const addItem = (item)=>{
+    setItems(items=> [...items,item]);
+  }
+
   return (
-    <div className="App justify-center p-4">
-      <h1 className="text-3xl font-bold border-b w-full text-center pb-6 border-white">
+    <div className="App justify-center p-4 max-w-[600px] mx-auto">
+
+      <h1 className="text-3xl font-bold border-b w-full text-center pb-6 border-gray-400">
         TODO LIST
       </h1>
 
-      <ItemInput />
-      <ItemList />
+      <div className="flex flex-col gap-4 p-4">
+        <ItemInput onAddItem={addItem} />
+        <ItemList />
+      </div>
     </div>
   );
 }
--- src/ItemInput.jsx
+++ src/ItemInput.jsx
@@ -1,3 +1,21 @@
-export default function ItemInput() {
-  return <div className="item-input">item input</div>;
+import { useState } from "react";
+
+export default function ItemInput({onAddItem}) {
+  const [inputValue,setInputValue] = useState("")
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    const trimmed = inputValue.trim();
+    if (!trimmed) return;
+
+    const item = {id: crypto.randomUUID(), text: trimmed, done: false}
+    onAddItem(item);
+    setInputValue("");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="item-input flex flex-col">
+      <input value={inputValue} onChange={e=> setInputValue(e.target.value)} type="text" placeholder="Add item ..." className="p-2 rounded-md border border-gray-300"/>
+      <button className="mr-auto mt-2 bg-black text-white py-1 px-2 rounded-md hover:bg-gray-700 transition-colors cursor-pointer">Add</button>
+    </form>
+  );
 }
