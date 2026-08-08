24e607d3921a00646f7b8c8310a179690508a6e4
Mohamed-Ashraf186 <mohamedashraf28812@gmail.com>
2026-08-08 14:38:36 +0300

    Remove commented-out code in handleCoinFlip function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ function App() {
   const [tails, setTails] = useState(0);
 
   function handleCoinFlip() {
-    // i have 2 images one heads and one tails.. when I press the flip me button I need it to give one of those two options.. so it will choose randomly from just one of those two..
-    // so maybe I can have an array of two objects.. one for heads and one for tails.. each one will have its own id.. one will have heads as an id and the other one will have tails
-    // and each object will also have the image path.. so when the button is pressed we use the random method between 1 and 2 which and then display the image in the object that matches that id maybe usin .find or something
     const result = Math.floor(Math.random() * 2);
     setIndex(result);
     result === 0 ? setHeads((h) => h + 1) : setTails((t) => t + 1);
